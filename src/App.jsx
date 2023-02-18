2739435c7c7a7ea59d199dd637e5b88aadad88a3
Erick <98369791+justthebasic@users.noreply.github.com>
2023-02-18 17:33:32 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Offers from './components/Offers'
+import Plan from './components/Plan'
+import Rooms from './components/Rooms'
+import ImageSlide from './components/ImageSlide'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <Offers/>
+      <Plan/>
+      <Rooms/>
+      <ImageSlide/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
